148d53e7c2e72f74aa5020c127cdf18e72304c4b
chicken nugget <116309083+chickennu99et@users.noreply.github.com>
2023-02-21 13:04:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+var you = 0;
+var bot = 0;
+var draw = 0;
 function rock()
 {
+update();
   var rps = Math.floor(Math.random() * 4);
-  if(rps=1)
+  if(rps==1)
   {
-    alert("draw");
+    draw++;
+  }else if(rps==2)
+  {
+    you++;
+  }else{
+  bot++;
   }
+}
+function update() {
+document.getElementById("botscore").innerText = "the bots wins:" + bot;
+document.getElementById("draw").innerText = "draws:" + draw;
+
+document.getElementById("youscore").innerText = "your wins:" + you;
 }
